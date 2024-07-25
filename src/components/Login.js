d12485e8b6361a6aeceebdca5ee7723f86756816
RohitParmar-17 <159684528+RohitParmar-17@users.noreply.github.com>
2024-07-25 20:52:18 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = (props) => {
     const handleSubmit = async (e) =>{
         e.preventDefault();
         // Api Call
-        const response = await fetch(`${host}/api/auth/login`, {
+        const response = await fetch('http://localhost:5000/api/auth/login', {
             method: "POST",
             headers: {
             "Content-Type": "application/json",
